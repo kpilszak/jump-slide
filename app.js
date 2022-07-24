c665141dfc458786a6bec4fd4332b4f4320109e9
kpilszak <klaudia.pilszak@gmail.com>
2022-07-24 09:58:45 +0200

    Add gravity and prevent multiple jumps

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
     const prince = document.querySelector('.character')
     let bottom = 0
+    let gravity = 0.9
+    let isJumping = false
 
     function jump() {
+        if (isJumping) return
+
         let timerUpId = setInterval(function() {
             if (bottom > 250) {
                 clearInterval(timerUpId)
                 let timerDownId = setInterval(function() {
                     if (bottom < 0) {
                         clearInterval(timerDownId)
+                        isJumping = false
                     }
                     bottom -= 5
                     prince.style.bottom = bottom + 'px'
                 }, 20)
             }
-
+            isJumping = true
             bottom += 30
+            bottom *= gravity
             prince.style.bottom = bottom + 'px'
         }, 20)
     }
