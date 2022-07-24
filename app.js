ac39b8e115bb940cffb84d85a066528cceef1da6
kpilszak <klaudia.pilszak@gmail.com>
2022-07-24 10:09:43 +0200

    Add slide

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ document.addEventListener('DOMContentLoaded', () => {
     let bottom = 0
     let gravity = 0.9
     let isJumping = false
+    let isGoingLeft = false
+    let isGoingRight = false
+    let left = 0
+    let leftTimerId
+    let rightTimerId
 
     function jump() {
         if (isJumping) return
+        
+        prince.classList.add('character')
+        prince.classList.remove('character-slide')
 
         let timerUpId = setInterval(function() {
             if (bottom > 250) {
@@ -26,9 +34,45 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 20)
     }
     
+    function slideLeft() {
+        prince.classList.add('character-slide')
+        prince.classList.remove('character')
+
+        if (isGoingRight) {
+            clearInterval(rightTimerId)
+            isGoingRight = false
+        }
+
+        isGoingLeft = true
+        leftTimerId = setInterval(function() {
+            left -= 5
+            prince.style.left = left + 'px'
+        }, 20)
+    }
+    
+    function slideRight() {
+        prince.classList.add('character-slide')
+        prince.classList.remove('character')
+
+        if (isGoingLeft) {
+            clearInterval(leftTimerId)
+            isGoingLeft = false
+        }
+
+        isGoingRight = true
+        rightTimerId = setInterval(function() {
+            left += 5
+            prince.style.left = left + 'px'
+        }, 20)
+    }
+
     function control(e) {
-        if (e.keyCode === 38) { // UP-ARROW
+        if (e.keyCode === 38) { // UP ARROW
             jump()
+        } else if (e.keyCode === 37) { // LEFT ARROW
+            slideLeft()
+        } else if (e.keyCode === 39) { // RIGHT ARROW
+            slideRight()
         }
     }
 
